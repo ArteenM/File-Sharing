24a11a3513ef2d9628415b7b233c278b4270278e
ArteenM <arteenmo1@github.com>
2025-08-01 17:54:58 -0700

    Finished authentication system for user passwords, next step is to setup a database to hold user information and tokens. Afterwards the online file sharing elements will come in

--- server.js
+++ server.js
@@ -1,71 +1,40 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 app.use(express.json())
 
-const users = []
 
-// First Route
+const posts = [
+    {
+        username: 'User1',
+        title: 'Post 1'
+    },
+    {
+        username: 'User2',
+        title: 'Post 2'
+    }
+]
 
-app.get('/users', (req, res) => {
-    res.json(users)
+app.get('/posts', authenticateToken, async (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
 })
 
+// This is called a middleware?
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    // Bearer TOKEN
+    const token = authHeader && authHeader.split(' ')[1]
 
-// Create users
+    if (token == null) return res.sendStatus(401)
 
-app.post('/users', async (req, res) => {
-    try
-    {
-        // Must await salt since bcrypt is asynchronous.
-        const salt = await bcrypt.genSalt()
-        const hashedPassword = await bcrypt.hash(req.body.password, salt)
-
-
-        // Can also do this:
-        // const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        // genSalt() defaults to 10 rounds, bcrypt.hash can automatically do it for you.
-
-
-        const user = { name: req.body.name, password: hashedPassword }
-        users.push(user)
-        res.status(201).send() // Successful 
-    }
-
-    catch
-    {
-        res.status(500).send() // Internal server error
-    }
-    
-})
-
-app.post('/users/login', async (req, res) => {
-    // Make async since using bcrypt to compare passwords.
-
-    const user = users.find(user => user.name = req.body.name)
-    if (user == null)
-    {
-        return res.status(400).send('Cannot find user') // Bad request
-    }
-
-    try
-    {
-        // Use bcrypt to prevent timing attacks in constant time algorithms
-        if (await bcrypt.compare(req.body.password, user.password))
-        {
-            res.status(201).send('Success')
-        }
-
-        else
-        {
-            res.status(400).send('Not allowed') // Bad request
-        }    
-    }
-
-    catch
-    {
-        res.status(500).send() // Internal server error
-    }
-})
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403) // No longer valid token
+        req.user = user
+        next()
+    })
+}
 app.listen(3000)
