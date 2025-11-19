207c654b0430e0f845fa59cff7d042a957994f3d
ArteenM <arteenmo1@github.com>
2025-11-18 16:50:06 -0800

    Added updated URLS for backend

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,7 +1,7 @@
 const config = {
   API_URL: window.location.hostname === 'localhost'
     ? 'http://localhost:4000'
-    : 'https://p2p-file-transfer-backend.onrender.com/'
+    : 'https://p2p-file-transfer-backend.onrender.com'
 }
 
 export default config
